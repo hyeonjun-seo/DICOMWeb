4f9ea73220bd0f31a828caddc6c79e064ea2034f
noah <noah@irm.kr>
2025-05-26 15:43:47 +0900

    Make it debuggable as Flask

--- app.py
+++ app.py
@@ -1,12 +1,49 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models import db, Todo
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:5432/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+# Create tables on startup
+with app.app_context():
+    db.create_all()
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    todos = Todo.query.all()
+    return jsonify([todo.to_dict() for todo in todos])
+
+
+@app.route('/todos', methods=['POST'])
+def create_todo():
+    data = request.get_json()
+    new_todo = Todo(title=data['title'], completed=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return jsonify(new_todo.to_dict()), 201
+
+
+@app.route('/todos/<int:todo_id>', methods=['PUT'])
+def update_todo(todo_id):
+    data = request.get_json()
+    todo = Todo.query.get_or_404(todo_id)
+    todo.title = data.get('title', todo.title)
+    todo.completed = data.get('completed', todo.completed)
+    db.session.commit()
+    return jsonify(todo.to_dict())
+
+
+@app.route('/todos/<int:todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    todo = Todo.query.get_or_404(todo_id)
+    db.session.delete(todo)
+    db.session.commit()
+    return '', 204
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
